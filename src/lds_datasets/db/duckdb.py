57a624676dc33d45df35eadfc63276871d7ac2c4
Travis Chambers <travis.chambers@numbersstation.ai>
2024-03-05 10:04:44 -1000

    chore: add queries for South America analysis

--- src/lds_datasets/db/duckdb.py
+++ src/lds_datasets/db/duckdb.py
@@ -1,11 +1,14 @@
 """Module for DuckDB database operations."""
 import duckdb
 
-stakes = duckdb.read_json("data/stakes_2024_02_04.json")
+stakes = duckdb.read_json("data/stakes_2024_03_05.json")
 stakes_added = duckdb.read_json("data/daily/*/stakes_added.json")
 stakes_removed = duckdb.read_json("data/daily/*/stakes_removed.json")
 
-wards = duckdb.read_json("data/wards_2024_02_04.json")
+districts_added = duckdb.read_json("data/daily/*/districts_added.json")
+districts_removed = duckdb.read_json("data/daily/*/districts_removed.json")
+
+wards = duckdb.read_json("data/wards_2024_03_05.json")
 wards_added = duckdb.read_json("data/daily/*/wards_added.json")
 wards_removed = duckdb.read_json("data/daily/*/wards_removed.json")
 
@@ -60,7 +63,6 @@ FULL OUTER JOIN
      GROUP BY address.state) r ON a.state = r.state
 ORDER BY net_change ASC;
 """)
-
 
 # combined wards added, removed, and net change per state in the USA
 net_change_usa_wards = duckdb.sql("""
@@ -124,4 +126,88 @@ JOIN
    FROM wards
    GROUP BY address.country) w ON s.country = w.country
 ORDER BY s.stake_count DESC;
+""")
+
+# combined stakes added, removed, and net change per country in South America
+net_change_south_america_stakes = duckdb.sql("""
+SELECT COALESCE(a.country, r.country) AS country,
+                COALESCE(a.added, 0) AS added,
+                COALESCE(r.removed, 0) AS removed,
+                COALESCE(a.added, 0) - COALESCE(r.removed, 0) AS net_change
+FROM
+       (SELECT address.country,
+                     COUNT(*) AS added
+              FROM stakes_added
+              WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+              GROUP BY address.country) a
+FULL OUTER JOIN
+       (SELECT address.country,
+                     COUNT(*) AS removed
+              FROM stakes_removed
+              WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+              GROUP BY address.country) r ON a.country = r.country
+ORDER BY net_change ASC;
+""")
+
+# combined districts added, removed, and net change per country in South America
+net_change_south_america_districts = duckdb.sql("""
+SELECT COALESCE(a.country, r.country) AS country,
+                     COALESCE(a.added, 0) AS added,
+                     COALESCE(r.removed, 0) AS removed,
+                     COALESCE(a.added, 0) - COALESCE(r.removed, 0) AS net_change
+FROM
+       (SELECT address.country,
+                     COUNT(*) AS added
+              FROM districts_added
+              WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+              GROUP BY address.country) a
+FULL OUTER JOIN
+       (SELECT address.country,
+                     COUNT(*) AS removed
+              FROM districts_removed
+              WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+              GROUP BY address.country) r ON a.country = r.country
+ORDER BY net_change ASC;
+""")
+
+# combined wards added, removed, and net change per country in South America
+net_change_south_america_wards = duckdb.sql("""
+SELECT COALESCE(a.country, r.country) AS country,
+                COALESCE(a.added, 0) AS added,
+                COALESCE(r.removed, 0) AS removed,
+                COALESCE(a.added, 0) - COALESCE(r.removed, 0) AS net_change
+FROM
+       (SELECT address.country,
+                     COUNT(*) AS added
+              FROM wards_added
+              WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+              GROUP BY address.country) a
+FULL OUTER JOIN
+       (SELECT address.country,
+                     COUNT(*) AS removed
+              FROM wards_removed
+              WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+              GROUP BY address.country) r ON a.country = r.country
+ORDER BY net_change ASC;
+""")
+
+# combined branches added, removed, and net change per country in South America
+net_change_south_america_branches = duckdb.sql("""
+SELECT COALESCE(a.country, r.country) AS country,
+            COALESCE(a.added, 0) AS added,
+            COALESCE(r.removed, 0) AS removed,
+            COALESCE(a.added, 0) - COALESCE(r.removed, 0) AS net_change
+FROM
+       (SELECT address.country,
+              COUNT(*) AS added
+        FROM branches_added
+        WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+        GROUP BY address.country) a
+FULL OUTER JOIN
+       (SELECT address.country,
+              COUNT(*) AS removed
+        FROM branches_removed
+        WHERE address.country in ('Colombia', 'Venezuela', 'Peru', 'Brazil', 'Chile', 'Argentina', 'Ecuador', 'Bolivia', 'Paraguay', 'Uruguay', 'Guyana', 'Suriname', 'French Guiana', 'Falkland Islands')
+        GROUP BY address.country) r ON a.country = r.country
+ORDER BY net_change ASC;
 """)
